6ac30e3325f3a08bc3d800b614cb885ab79665b3
inudzuka-mitsu <valusa2016@gmail.com>
2021-11-17 16:14:17 -0600

    cucumber options name change

--- src/test/java/com/testproject/runners/TestRunner.java
+++ src/test/java/com/testproject/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber-reports/CucumberTestReport.json",
+        plugin = {"json:target/cucumber.json",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "pretty"},
